57370e75caf049f153fa64a5ceeb31708d4b4cfd
Josh English <josh@joshenglish.com>
2016-04-18 15:50:15 -0500

    Created .gitignore for .env file.  Will send .env to slack for everyone.  App.js has been edited to include the database.js which is our connection to MongoDB.  Schema.js includes schema for User, Location and Image.  The package.json has been updated to include the dotenv and mongoose modules.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// require database
+require('./db/database');
 
 var controllers = require('./controllers/index');
 var users = require('./controllers/users');
---
+++ models/schema.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+// userSchema
+
+var userSchema = new.mongoose.Schema({
+  username: String,
+  email: String,
+  passwordHash: String
+});
+
+mongoose.model('User', userSchema);
+
+// locationSchema
+
+var locationSchema = new.mongoose.Schema({
+  location_id: Number,
+  coordinates: String,
+  art_piece_name: String,
+  artist: [],
+  description: String,
+  directions: String
+});
+
+mongoose.model('Location', locationSchema);
+
+// imageSchema
+
+var imageSchema = new.mongoose.Schema({
+  username: String,
+  location_id: String,
+  date: Date,
+  image: String
+});
+
+mongoose.model('Image', imageSchema);
+
+module.exports = {
+  User: User,
+  Location: Location,
+  Image: Image
+};
+
