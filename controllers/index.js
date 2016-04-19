b2ff8128b0ea01bcfb96a932fa983fef2e416a93
Alex Kim <alexander.e.kim@gmail.com>
2016-04-19 12:14:46 -0500

    added james user controller and small change on indexcontroller. also added empty controller files

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var controller = express.Router();
+var http = require('http');
 
 var models = require('../models/schema')
 
 /* GET home page. */
 controller.get('/', function(req, res, next) {
   // res.render('index', { title: 'ChiStreetArt' });
-
 });
+
+controller.post('/', function(req, res, next){
+  http.req()
+})
+
 
 module.exports = controller;
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,63 @@
 var express = require('express');
-var router = express.Router();
+var controller = express.Router();
+var UserAccount = require('../models/UserAccount');
+var bcrypt = require('bcrypt');
+var Salt = bcrypt.genSaltSync(10);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+controller.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+controller.get('/logout', function(req, res, next) {
+  req.session.user = null;
+  res.json({ 'message': 'You have been logged out.'});
+});
+
+// CREATE an account
+controller.post('/register', function(req, res, next) {
+  /*{
+    name: String,
+    email: String,
+    passwordHash: String,
+    birthDate: String
+  }*/
+  // todo: data validation
+  var userInfo = {
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, Salt),  // soon need to hash the user's password
+    birthDate: req.body.birthdate
+  };
+  UserAccount.create(userInfo, function(error, user) {
+    console.log('/users/register =>');
+    console.log(user);
+    res.json({ 'message': 'You have successfully registered an account!'});
+  });
+});
+
+controller.post('/login', function(req, res, next) {
+  //bcrypt.compareSync
+  var userInfo = {
+    email: req.body.email,
+    password: req.body.password
+  };
+  UserAccount.find({ email: userInfo.email }, function(err, user) {
+    // if we find our user.. compare passwords!
+    console.log(userInfo);
+    console.log(user);
+    console.log(userInfo.password);
+    var isPasswordValid = bcrypt.compareSync(userInfo.password, user[0].passwordHash);
+    if (isPasswordValid) {
+      // log user in
+      console.log(req.session);
+      req.session.user = user[0].email;
+      console.log(req.session);
+      res.json({ 'message': 'Logged in successfully'});
+    } else {
+      res.json({ 'message': 'Invalid username and/or password'});
+    }
+  });
+});
+
+module.exports = controller;
