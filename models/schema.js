8f552b5d66174a10dba6d71a82d3792bea056593
Josh English <josh@joshenglish.com>
2016-04-19 09:52:11 -0500

    Updating Schema file to only include User Schema.  Moved other items to API Server

--- models/schema.js
+++ models/schema.js
@@ -10,33 +10,7 @@ var userSchema = new mongoose.Schema({
 
 var User = mongoose.model('User', userSchema);
 
-// locationSchema
-
-var locationSchema = new mongoose.Schema({
-  location_id: Number,
-  coordinates: String,
-  art_piece_name: String,
-  artist: [],
-  description: String,
-  directions: String
-});
-
-var Location = mongoose.model('Location', locationSchema);
-
-// imageSchema
-
-var imageSchema = new mongoose.Schema({
-  username: String,
-  location_id: String,
-  date: Date,
-  image: String
-});
-
-var Image = mongoose.model('Image', imageSchema);
-
 module.exports = {
   User: User,
-  Location: Location,
-  Image: Image
 };
 
