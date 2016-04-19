11053377ce49bc82c6659ff532e6a55ae7d78a58
Alex Kim <alexander.e.kim@gmail.com>
2016-04-19 16:23:14 -0500

    working for now requests

--- controllers/locations.js
+++ controllers/locations.js
@@ -6,8 +6,26 @@ var models = require('../models/schema')
 
 /* GET home page. */
 controller.get('/', function(req, res, next) {
-  res.render('locations', { title: 'Locations' });
-});
+  // res.render('locations', { title: 'Locations' });
+  var request = http.get("http://localhost:5000/locations", function(response){
+    console.log(response.statusCode);
+    var body = "";
+    var location = "";
+    response.on('data', function(chunk){
+      console.log("body: " + chunk);
+      console.log(body)
+      body += chunk;
+    })
+    response.on('end', function(){
+      console.log("no more data")
+      if (response.statusCode == 200){
+        location = JSON.parse(body);
+        console.log(location);
+        res.json(location)
+      }
+    })
+  })
+})
 
 controller.post('/', function(req, res, next){
   http.req()
