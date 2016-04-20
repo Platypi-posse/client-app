6d30cc136f13190124a8b5decbfc97347cf9c305
Josh English <josh@joshenglish.com>
2016-04-20 10:58:35 -0500

    Working Users controller without any validation.  Will continue to confirm validation over the day

--- controllers/users.js
+++ controllers/users.js
@@ -1,63 +1,67 @@
 var express = require('express');
 var controller = express.Router();
-// var UserAccount = require('../models/schema');
-// var bcrypt = require('bcrypt');
-// var Salt = bcrypt.genSaltSync(10);
+var UserAccount = require('../models/schema');
+var bcrypt = require('bcrypt');
+var Salt = bcrypt.genSaltSync(10);
 
 /* GET users listing. */
 controller.get('/', function(req, res, next) {
   res.render('loginregister', { title: 'Locations' });
 });
-//
-// controller.get('/logout', function(req, res, next) {
-//   req.session.user = null;
-//   res.json({ 'message': 'You have been logged out.'});
-// });
-//
-// // CREATE an account
-// controller.post('/register', function(req, res, next) {
-//   /*{
-//     name: String,
-//     email: String,
-//     passwordHash: String,
-//     birthDate: String
-//   }*/
-//   // todo: data validation
-//   var userInfo = {
-//     name: req.body.name,
-//     email: req.body.email,
-//     passwordHash: bcrypt.hashSync(req.body.password, Salt),  // soon need to hash the user's password
-//     birthDate: req.body.birthdate
-//   };
-//   UserAccount.create(userInfo, function(error, user) {
-//     console.log('/users/register =>');
-//     console.log(user);
-//     res.json({ 'message': 'You have successfully registered an account!'});
-//   });
-// });
-//
-// controller.post('/login', function(req, res, next) {
-//   //bcrypt.compareSync
-//   var userInfo = {
-//     email: req.body.email,
-//     password: req.body.password
-//   };
-//   UserAccount.find({ email: userInfo.email }, function(err, user) {
-//     // if we find our user.. compare passwords!
-//     console.log(userInfo);
-//     console.log(user);
-//     console.log(userInfo.password);
-//     var isPasswordValid = bcrypt.compareSync(userInfo.password, user[0].passwordHash);
-//     if (isPasswordValid) {
-//       // log user in
-//       console.log(req.session);
-//       req.session.user = user[0].email;
-//       console.log(req.session);
-//       res.json({ 'message': 'Logged in successfully'});
-//     } else {
-//       res.json({ 'message': 'Invalid username and/or password'});
-//     }
-//   });
-// });
-//
+
+controller.get('/logout', function(req, res, next) {
+  req.session.user = null;
+  res.json({ 'message': 'You have been logged out.'});
+});
+
+// CREATE an account
+controller.post('/register', function(req, res, next) {
+  /*{
+    name: String,
+    email: String,
+    passwordHash: String,
+  }*/
+  // todo: data validation
+  var userInfo = {
+    username: req.body.username,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, Salt),  // soon need to hash the user's password
+  };
+  var user = new UserAccount.User({ username: req.body.username, email: req.body.email, passwordHash: req.body.password});
+  console.log(user);
+  user.save(function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.json({'message': 'You have successfully registered an account!'})
+    }
+  });
+  // UserAccount.create(userInfo, function(error, user) {
+  //   console.log('/users/register =>');
+  //   console.log(user);
+  //   res.json({ 'message': 'You have successfully registered an account!'});
+  // });
+});
+
+controller.post('/login', function(req, res, next) {
+  //bcrypt.compareSync
+  var userInfo = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  UserAccount.findOne({ email: userInfo.email }, function(err, user) {
+    // if we find our user.. compare passwords!
+    var isPasswordValid = bcrypt.compareSync(userInfo.password, user.passwordHash);
+    if (isPasswordValid) {
+      // log user in
+      console.log(req.session);
+      req.session.user = user.username;
+      console.log(req.session);
+      res.json({ 'message': 'Logged in successfully'});
+    } else {
+      res.json({ 'message': 'Invalid username and/or password'});
+    }
+  });
+});
+
 module.exports = controller;
