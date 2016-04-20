926c12d8ec03f9612cf27e7b09d5cc1ad19032d5
Alex Kim <alexander.e.kim@gmail.com>
2016-04-20 10:56:41 -0500

    added features to locations controller allowing search specific location by id by using route. also redirects to err page. added few changes to locations view as well as error view. added images controller to be worked on.

--- controllers/index.js
+++ controllers/index.js
@@ -9,6 +9,10 @@ controller.get('/', function(req, res, next) {
   res.render('index', { title: 'ChiStreetArt' });
 });
 
+controller.get('/about', function(req,res, next){
+  res.render('about', { title: 'About Us'})
+})
+
 controller.post('/', function(req, res, next){
   http.req()
 })
