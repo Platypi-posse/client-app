316c32aa40e75cb68d2f3ba736139ad90f22e3a9
Josh English <josh@joshenglish.com>
2016-04-21 17:01:16 -0500

    Updated the console.logs and added some fixes

--- controllers/locations.js
+++ controllers/locations.js
@@ -4,24 +4,26 @@ var http = require('http');
 
 // var models = require('../models/schema')
 
+var linkRequest = process.env.Link || "http://localhost:5000";
+
 /* GET home page. */
 controller.get('/', function(req, res, next) {
   // res.render('locations', { title: 'Locations' });
-  var request = http.get("http://localhost:5000/locations/images", function(response){
-    console.log(response.statusCode);
+  var request = http.get(linkRequest + "/locations/images", function(response){
+    // console.log(response.statusCode);
     var body = "";
     var location = "";
     response.on('data', function(chunk){
-      console.log("body: " + chunk);
-      console.log(body)
+      // console.log("body: " + chunk);
+      // console.log(body)
       body += chunk;
     })
     response.on('end', function(){
-      console.log("no more data")
+      // console.log("no more data")
       if (response.statusCode == 200){
         location = JSON.parse(body);
-        console.log("location here:")
-        console.log(location);
+        // console.log("location here:")
+        // console.log(location);
         // console.log("location[0] here:")
         // console.log(location[0]);
         // res.json(location)
@@ -39,21 +41,23 @@ controller.get('/', function(req, res, next) {
 controller.get('/:id', function(req, res, next) {
   // res.render('locations', { title: 'Locations' });
   var id = req.params.id;
-  var request = http.get("http://localhost:5000/locations/" + id, function(response){
-    console.log(response.statusCode);
+  console.log('this is line 44');
+  console.log(linkRequest + "/locations/" + id);
+  var request = http.get(linkRequest + "/locations/" + id, function(response){
+    // console.log(response.statusCode);
     var body = "";
     var location = "";
     response.on('data', function(chunk){
-      console.log("body: " + chunk);
-      console.log(body)
+      // console.log("body: " + chunk);
+      // console.log(body)
       body += chunk;
     })
     response.on('end', function(){
-      console.log("no more data")
+      // console.log("no more data")
       if (response.statusCode == 200){
         location = JSON.parse(body);
-        console.log("location here:")
-        console.log(location);
+        // console.log("location here:")
+        // console.log(location);
         if (location === null) {
           var err = new Error('Not Found');
           err.status = 404;
@@ -61,15 +65,15 @@ controller.get('/:id', function(req, res, next) {
         }
         else {
           intersection = location.intersection;
-          console.log(intersection);
+          // console.log(intersection);
           art_piece_name = location.art_piece_name;
-          console.log(art_piece_name);
+          // console.log(art_piece_name);
           description = location.description;
-          console.log(description)
+          // console.log(description)
           directions = location.directions;
-          console.log(directions);
+          // console.log(directions);
           artist = location.artist;
-          console.log(artist);
+          // console.log(artist);
           image = location.image;
           // res.json(location);
           res.render('locations', { display: location, image: image, title: 'Art Catalog', location: intersection, art_piece_name: art_piece_name, description: description, directions: directions, artist: artist })
@@ -89,8 +93,8 @@ controller.post('/', function(req, res, next){
     directions: req.body.directions
   });
   var options = {
-    hostname: "localhost",
-    port: 5000,
+    hostname: process.env.Link || "localhost",
+    port: process.env.Port || 5000,
     path: "/locations/post",
     method: "POST",
     headers: {
@@ -99,19 +103,19 @@ controller.post('/', function(req, res, next){
     }
   };
   var request = http.request(options, function(response){
-    console.log(response.statusCode);
+    // console.log(response.statusCode);
     var body = "";
     var message = "";
     response.on('data', function(chunk){
-      console.log("body: " + chunk);
-      console.log(body)
+      // console.log("body: " + chunk);
+      // console.log(body)
       body += chunk;
     })
     response.on('end', function(){
       console.log("no more data")
       if (response.statusCode == 200){
         message = JSON.parse(body);
-        console.log(message);
+        // console.log(message);
         res.json(message)
       }
     })
